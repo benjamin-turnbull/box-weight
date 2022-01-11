bd9657e6699334d96114d4ad02ac10f0fc863517
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-11 12:51:04 +1030

    condensed code by performing calculation in prinft statement instead of creating new variable

--- box-weight.c
+++ box-weight.c
@@ -1,6 +1,6 @@
-/* 
+/*
     Shipping companies base their shipping fee on the dimensional weight of a box which exceeds it's actual weight.
-    The typical method of calculating dimensional weight is dividing the volume by 166. 
+    The typical method of calculating dimensional weight is dividing the volume by 166.
     This program will compute the dimensional weight of a 12" x 10" x 8" box in pounds.
 */
 
@@ -11,16 +11,15 @@ int main(void)
     int height, length, width, volume, weight;
 
     printf("Enter height of box: \n");
-    scanf(" %d", &height);
+    scanf("%d", &height);
     printf("Enter length of the box: \n");
-    scanf(" %d", &length);
+    scanf("%d", &length);
     printf("Enter width of the box: \n");
-    scanf(" %d", &width);
+    scanf("%d", &width);
     volume = height * length * width;
-    weight = (volume +165) / 166; /* C by default rounds down, adding 165 ensures we are rounding up */
 
     printf("Volume (cubic inches): %d\n", volume);
-    printf("Dimensional weight (pounds): %d\n", weight);
+    printf("Dimensional weight (pounds): %d\n", volume+165);
 
     return 0;
 }
